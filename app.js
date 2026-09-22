5878612dae99e44dd99600c9cca0ee6140c103de
Mr.AsemZ <alzaghal.asem@gmail.com>
2026-09-22 10:15:34 +0300

    first done

--- app.js
+++ app.js
@@ -10,3 +10,11 @@ else if (membership.toLocaleLowerCase() == "regular"){
 else{
      alert("Welcome, " + userName + "!");
 }
+
+let genre = prompt("Do you prefer fiction or non-fiction?");
+
+let bookTitle = prompt("What is the title of the book you look to borrow?");
+
+alert("Alright sir the book '" + bookTitle + "' is being reserved for you!");
+
+console.log("The user " + userName + "has ordered the book: " + bookTitle);
