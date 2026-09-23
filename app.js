ceac5c76921d7a51b4c5da8fec2be8ad721a3d28
Mr.AsemZ <alzaghal.asem@gmail.com>
2026-09-23 15:40:00 +0300

    bookshop new functions

--- app.js
+++ app.js
@@ -1,20 +1,47 @@
-let userName = prompt("What is your name?");
-let membership = prompt("Enter your membership type: Student or Regular");
+function collectUserData(){
+    //username
+    let userName = prompt("What is your name?");
 
-if (membership.toLowerCase() == "student"){
-    alert("Welcome, " + userName + "! You joined as Scholar");
+    //membership type
+    let membershipType = prompt("Enter your membership type: Student or Regular");
+    if (membershipType.toLowerCase() == "student"){
+        alert("Welcome, " + userName + "! You joined as Scholar");
+    }
+    else if (membershipType.toLocaleLowerCase() == "regular"){
+            alert("Welcome, " + userName + "! You joined as Member");
+    }
+    else{
+        askForMembership();
+        //  alert("Welcome, " + userName + "!");
+    }
+
+    //book genre
+    let bookGenre = prompt("Do you prefer fiction or non-fiction?");
+
+    //booktitle
+    let bookTitle = prompt("What is the title of the book you look to borrow?");
+
+    return [userName, membershipType, bookGenre, bookTitle];
 }
-else if (membership.toLocaleLowerCase() == "regular"){
-        alert("Welcome, " + userName + "! You joined as Member");
+
+function askForMembership(){
+
+    let membershipType = prompt("The membership type must be either: Student or Regular");
+    while (membershipType.toLowerCase() != "student" &&
+           membershipType.toLowerCase() != "regular" ){
+        membershipType = prompt("The membership type must be either: Student or Regular !");
+    }
+    
+    return membershipType;
 }
-else{
-     alert("Welcome, " + userName + "!");
+
+
+// alert("Alright sir the book '" + bookTitle + "' is being reserved for you!");
+// console.log("The user " + userName + " has ordered the book: " + bookTitle);
+
+const order = collectUserData();
+for (let i = 0; i <=3; i++){
+    console.log(order[i]);
 }
 
-let genre = prompt("Do you prefer fiction or non-fiction?");
 
-let bookTitle = prompt("What is the title of the book you look to borrow?");
-
-alert("Alright sir the book '" + bookTitle + "' is being reserved for you!");
-
-console.log("The user " + userName + "has ordered the book: " + bookTitle);
